7cb7cfb6ec98cbf020e26c874082332880365c91
Richard Franks <git@rf152.co.uk>
2026-07-21 20:52:14 +0100

    Updated easy-setup.sh

--- easy-setup.sh
+++ easy-setup.sh
@@ -3,7 +3,7 @@
 set -eu
 
 sudo apt update
-sudo apt install -y libconfig++-dev libboost-program-options-dev libavahi-client3 cmake libcamera-dev libevent-pthreads-2.1-7 libevent-core-2.1-7 libevent-dev
+sudo apt install -y libconfig++-dev libboost-program-options-dev libavahi-client3 cmake libcamera-dev libevent-pthreads-2.1-7 libevent-core-2.1-7 libevent-dev libcamera0 libcamera-tools libcamera-apps-lite librpicam-app1 librpicam-app-dev
 
 ./build.sh
 sudo ./install.sh
